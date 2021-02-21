cacda67d0fd5f091d9323f5c4c05fd3ccd581339
iotanum <evalduuk@gmail.com>
2021-02-22 00:41:36 +0200

    Downsize built exe size

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['scriptukas.py'])
+setup(console=['scriptukas.py'],
+      zipfile=None,
+      options={
+          "py2exe": {
+              "optimize": 2,
+              "bundle_files": 1,
+              "compressed": True,
+              "dll_excludes": ['msvcr71.dll'],
+              "excludes": ['pyreadline', 'difflib', 'doctest', 'locale',
+                           'optparse', 'pickle', 'calendar']
+          }
+      })
