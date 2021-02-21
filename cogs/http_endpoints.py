a0bea7fd1794ba410ea14720479768dab3537048
iotanum <evalduuk@gmail.com>
2021-02-21 21:26:51 +0200

    Modify http server so it would be inscope of bot

--- cogs/http_endpoints.py
+++ cogs/http_endpoints.py
@@ -1,9 +1,39 @@
 from aiohttp import web
+from discord.ext import commands
+import aiohttp
+from aiohttp import web
+
+import os
 
 
-routes = web.RouteTableDef()
+HTTP_SERVER_PORT = os.getenv("HTTP_SERVER_PORT")
 
 
-@routes.get('/get_picks')
-async def response(request):
-    return web.Response(text="Goodbyes, world")
+class HTTPServer(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def http_server(self):
+        async def get_handler(request):
+            return web.Response(text=f"{self.bot.user.name}")
+
+        async def post_handler(request):
+            body = await request.text()
+            return web.Response(text=body)
+
+        app = web.Application()
+        # app.router.add_get("/get", get_handler)
+        app.router.add_post("/get", post_handler)
+
+        runner = aiohttp.web.AppRunner(app)
+        await runner.setup()
+        site = aiohttp.web.TCPSite(runner, port=HTTP_SERVER_PORT)
+        await self.bot.wait_until_ready()
+        await site.start()
+        print(f"HTTP server running on: {'0.0.0.0' if not site._host else site._host}:{site._port} ")
+
+
+def setup(bot):
+    server = HTTPServer(bot)
+    bot.add_cog(server)
+    bot.loop.create_task(server.http_server())
--- launcher.py
+++ launcher.py
@@ -1,34 +1,19 @@
 import os
-import asyncio
 
 from dotenv import load_dotenv
 from discord.ext import commands
-import aiohttp
-from aiohttp import web
-
-from cogs.http_endpoints import routes
 
 
 if not os.getenv("DISCORD_API"):
     load_dotenv(dotenv_path="secrets.env")
 
-HTTP_SERVER_PORT = os.getenv("HTTP_SERVER_PORT")
 DISCORD_API_KEY = os.getenv("DISCORD_API")
 
 bot = commands.Bot(command_prefix=os.getenv("COMMAND_PREFIX"))
 
 initial_extensions = ['cogs.command_error_handler',
-                      'cogs.commands']
-
-
-async def run_http_server():
-    app = web.Application()
-    app.add_routes(routes)
-    runner = aiohttp.web.AppRunner(app)
-    await runner.setup()
-    site = aiohttp.web.TCPSite(runner, port=HTTP_SERVER_PORT)
-    await site.start()
-    print(f"HTTP server running on: {'0.0.0.0' if not site._host else site._host}:{site._port} ")
+                      'cogs.commands',
+                      'cogs.http_endpoints']
 
 
 @bot.event
@@ -46,5 +31,4 @@ if __name__ == '__main__':
             print(e)
             print(f'Failed to load extension "{extension}"')
 
-    asyncio.get_event_loop().run_until_complete(run_http_server())
     bot.run(DISCORD_API_KEY, bot=True, reconnect=True)
