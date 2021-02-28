3a1bee28a900708b275d3057f1fe39845fba4729
iotanum <evalduuk@gmail.com>
2021-02-28 20:33:08 +0200

    Add formatting to opgg players

--- cogs/helpers/opgg_players.py
+++ cogs/helpers/opgg_players.py
@@ -199,7 +199,7 @@ class Opgg(commands.Cog):
         op_gg_url = "op.gg/summoner/userName="
         active_players = await self.get_players()
         for region, player in active_players:
-            string += f"\n[https://{region}]({region}{op_gg_url}{active_players})"
+            string += f"\n[{region}](https://{region}{op_gg_url}{active_players})"
 
         await ctx.send(f"```\n{string}\n```")
 
