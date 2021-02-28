cc270feae90b003c6270c5ae91755fb71097cb43
iotanum <evalduuk@gmail.com>
2021-02-28 21:01:52 +0200

    Beautify string format

--- cogs/helpers/opgg_players.py
+++ cogs/helpers/opgg_players.py
@@ -206,7 +206,9 @@ class Opgg(commands.Cog):
         active_players = await self.get_players()
         for region, player in active_players.items():
             player = player.replace(" ", "+")
-            string += f"\n[{region}](https://{region}{op_gg_url}{player}) | {mapped_values[region]} | {player}"
+            hyperlink = f"[{region}](https://{region}{op_gg_url}{player})".ljust(5)
+            mapped_region = mapped_values[region].ljust(4)
+            string += f"\n{hyperlink}|{mapped_region}|{player}"
 
         embed.description = string
         await ctx.send(embed=embed)
